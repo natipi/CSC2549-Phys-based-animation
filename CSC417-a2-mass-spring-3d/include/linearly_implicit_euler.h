e0a2c136d95cdc7ae5ceabe0be5c5a85259e498e
natipi <nataliap@mit.edu>
2023-10-29 15:16:35 -0400

    Sparse matrix linear system now being solved successfully, but bunny not moving :(

--- CSC417-a2-mass-spring-3d/include/linearly_implicit_euler.h
+++ CSC417-a2-mass-spring-3d/include/linearly_implicit_euler.h
@@ -26,29 +26,39 @@ inline void linearly_implicit_euler(Eigen::VectorXd &q, Eigen::VectorXd &qdot, d
 
     debug2(tmp_force.norm());
 
-    Eigen::SparseLU<Eigen::SparseMatrix<double> > solver;
-
+    // Eigen::SparseLU<Eigen::SparseMatrix<double> > solver;
+    
     Eigen::SparseMatrix<double> a = mass - dt * dt * tmp_stiffness;
-    debug2("mass nonzeroes");debug2(mass.nonZeros());
-    debug2("stiffness nonzeroes");debug2(tmp_stiffness.nonZeros());
+    Eigen::SimplicialCholesky<Eigen::SparseMatrix<double> > solver(a);
 
+    // debug2("dt ");debug2(dt);
+    // debug2("mass nonzeroes");debug2(mass.nonZeros());
+    // debug2("stiffness nonzeroes");debug2(tmp_stiffness.nonZeros());
+    // int massrows = mass.rows();
+    // std::cout << "mass diagonal: ";
+    // for (int i = 0; i < massrows; i++) {
+    //     std::cout << mass.coeff(i,i) << " ";
+    // }
+    // debug(" ");
     // a.makeCompressed();
-    solver.compute(a);
+    // std::cout << "a matrix: " << a << std::endl << std::endl << std::endl;
+    
+    // solver.compute(a);
 
-    if(solver.info()!=Eigen::Success) {
-        if (solver.info()==Eigen::NumericalIssue)
-            debug2("numerical issue");
-        if (solver.info()==Eigen::InvalidInput)
-            debug2("invalid input ");
-        return;
-    }
+    // if(solver.info()!=Eigen::Success) {
+    //     if (solver.info()==Eigen::NumericalIssue)
+    //         debug2("numerical issue");
+    //     if (solver.info()==Eigen::InvalidInput)
+    //         debug2("invalid input ");
+    //     return;
+    // }
 
     qdot = solver.solve(mass * qdot + dt * tmp_force);
 
-    if(solver.info()!=Eigen::Success) {
-        debug2("solve failed");
-        return;
-    }
+    // if(solver.info()!=Eigen::Success) {
+    //     debug2("solve failed");
+    //     return;
+    // }
 
     q += dt * qdot; 
 }
